1e98f2593ae72336f928ecfdb32833a51f2566aa
alexsander kurtz <alexsander_kurtz@hotmail.com>
2024-01-14 11:25:49 -0300

    iniciei a criação de um programa de cadastro de clientes

---
+++ ListaDeCadastro/ListaDeCadastro/Cadastro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeCadastro
+{
+    internal class Cadastro
+    {
+        List <string> clientes = new List <string> ();
+
+        public void Adicionar(string cliente)
+        {
+            clientes.Add(cliente);
+        }
+
+        public void ListarClientes()
+        {
+            foreach(string cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+        }
+
+        public void RemoverCliente(string nome)
+        {
+            clientes.Remove(nome);
+        }
+        
+
+    }
+}
---
+++ ListaDeCadastro/ListaDeCadastro/Program.cs
@@ -0,0 +1,46 @@
+﻿using ListaDeCadastro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Cadastro cadastro = new Cadastro();
+        
+        bool programa = true;
+
+        while (programa) {
+
+            Console.WriteLine("Digite uma opção abaixo.");
+            Console.WriteLine("1-Adicionar Cliente.");
+            Console.WriteLine("2-Remover Cliente.");
+            Console.WriteLine("3-Listar Clientes.");
+            Console.WriteLine("4-Encerrar o programa.");
+
+            int escolha = Convert.ToInt32(Console.ReadLine());
+
+        if (escolha == 1) {
+
+            Console.WriteLine("Digite o nome do cliente");
+            string nomeCliete= Console.ReadLine();
+            cadastro.Adicionar(nomeCliete);
+                Console.WriteLine($"Cliente {nomeCliete} cadastrado com sucesso.");
+
+            }
+            else if (escolha == 2)
+        {
+            Console.WriteLine("Digite o nome do cliente");
+            string nomeCliete = Console.ReadLine();
+            cadastro.RemoverCliente(nomeCliete);
+                Console.WriteLine($"Cliente {nomeCliete} removido com sucesso.");
+            }
+            else if (escolha == 3)
+        {
+            cadastro.ListarClientes();
+        }else if (escolha == 4) 
+        {
+            Console.WriteLine("Obrigado por ultilizar o programa");
+                programa= false;
+        }
+        }
+    }
+}
